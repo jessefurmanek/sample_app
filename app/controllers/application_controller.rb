35ec440c9fc33575077e626eacdee2ce07c5d6f8
jessefurmanek <jesse.furmanek@gmail.com>
2015-04-01 12:25:15 -0500

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,13 @@ class ApplicationController < ActionController::Base
   	render text: "hello, world!"
   end
 
-   # Returns the full title on a per-page basis.
+     # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 
 end
